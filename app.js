cf12a34f763ec0558b23e9d7a609faefea1135f0
Jazmia Cornell <jazmiadc@gmail.com>
2025-03-26 00:56:41 -0400

    database test

--- app.js
+++ app.js
@@ -621,7 +621,7 @@ app.put("/put-trades-ajax", function (req, res, next) {
 });
 
 // LISTENER
-// db.sync();
+db.sync();
 app.listen(PORT, function () {
   // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
   console.log(
