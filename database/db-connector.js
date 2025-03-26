c0502850a752692f0fc4b473da628d4e3085a8d5
Jazmia Cornell <jazmiadc@gmail.com>
2025-03-26 00:58:49 -0400

    databast test

--- database/db-connector.js
+++ database/db-connector.js
@@ -5,12 +5,12 @@ var mysql = require("mysql");
 // Create a 'connection pool' using the provided credentials
 
 var pool = mysql.createPool({
-  connectionLimit: 10,
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT,
+  url: process.env.MYSQL_URL,
 });
 
 // Export it for use in you app
